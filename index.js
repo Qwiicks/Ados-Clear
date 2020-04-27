9220c1600c50f13466e56504ff70fc92d9f22964
Qwiicks <64379279+Qwiicks@users.noreply.github.com>
2020-04-27 17:56:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+let prefix = "!"
+ 
+client.login('NzAzNjI1MTEyNjk5NTM1NDEx.XqXMaw.8AoYLOY-yQjKB_J0iRvh01T7K18')
+
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + "vider") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        let count = parseInt(args[1])
+        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+        if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+        if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+        message.channel.bulkDelete(count + 1, true)
+    }
+ 
+    if (args[0].toLowerCase() === prefix + "mute") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.send("Membre introuvable")
+        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+        if (!member.manageable) return message.channel.send("Je ne peux pas mute ce membre")
+        let muterole = message.guild.roles.find(role => role.name === 'Muted')
+        if (muterole) {
+            member.addRole(muterole)
+            message.channel.send(member + ' a été mute :white_check_mark:')
+        }
+        else {
+            message.guild.createRole({name: 'Muted', permissions: 0}).then(function (role) {
+                message.guild.channels.filter(channel => channel.type === 'text').forEach(function (channel) {
+                    channel.overwritePermissions(role, {
+                        SEND_MESSAGES: false
+                    })
+                })
+                member.addRole(role)
+                message.channel.send(member + ' a été mute :white_check_mark:')
+            })
+        }
+    }
+})
